9c19ea53b4f6b39f7412817e008b84a66159dca2
sayan95 <sharthakmallik@gmail.com>
2021-08-07 01:03:13 +0530

    [Feature]: adds admin user interactions(create, fetch, etc)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('users', [UserController::class, 'index'])->name('user.all');
+Route::get('users/{id}', [UserController::class, 'show'])->name('user');
+Route::post('users/create', [UserController::class, 'create'])->name('user.create');
