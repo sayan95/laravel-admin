00c2dc781e86470e6d960cc3bedf24745932284d
sayan95 <sharthakmallik@gmail.com>
2021-08-09 21:05:48 +0530

    [Feature]: Adds onload product seeding to db action

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,6 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id'];
+    protected $fillable = ['title', 'description', 'image', 'price'];
 
 }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,7 +22,10 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->text(30),
+            'description' => $this->faker->text,
+            'image' => $this->faker->imageUrl(),
+            'price' => $this->faker->numberBetween(10, 100)
         ];
     }
 }
